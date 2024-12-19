a935f6d1d85b3397aba100b512f15e0bb10f65ca
SaimRazaAly <saimrazateach2010@gmail.com>
2024-12-18 19:01:17 -0800

    complted the about seciton

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      listStyleType: {
+        square: 'square', // Add support for square list bullets
+      },
+    },
   },
   plugins: [],
 };
